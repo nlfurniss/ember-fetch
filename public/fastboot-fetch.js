4502e75db498eaf894652d936aa440f5c7f45be1
Nathaniel Furniss <nlfurniss@gmail.com>
2017-09-08 03:26:56 -0700

    Guard FastBoot and FastBoot.require

--- public/fastboot-fetch.js
+++ public/fastboot-fetch.js
@@ -1,13 +1,15 @@
 (function() {
-  define('fetch', ['exports'], function(self) {
-    var fetch = FastBoot.require('node-fetch');
-    self['default'] = fetch;
-    self['Headers'] = fetch.Headers;
-    self['Request'] = fetch.Request;
-    self['Response'] = fetch.Response;
-  });
+  if (typeof FastBoot !== 'undefined' && FastBoot.require) {
+    define('fetch', ['exports'], function(self) {
+      var fetch = FastBoot.require('node-fetch');
+      self['default'] = fetch;
+      self['Headers'] = fetch.Headers;
+      self['Request'] = fetch.Request;
+      self['Response'] = fetch.Response;
+    });
 
-  define('fetch/ajax', ['exports'], function() {
-    throw new Error('You included `fetch/ajax` but it was renamed to `ember-fetch/ajax`');
-  });
+    define('fetch/ajax', ['exports'], function() {
+      throw new Error('You included `fetch/ajax` but it was renamed to `ember-fetch/ajax`');
+    });
+  }
 })();
